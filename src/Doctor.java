8dc915ebb51ef7e35df0a0da8b5ded34c1088973
Alex Acevedo <alex.acpe@outlook.com>
2022-06-23 16:30:01 -0500

    Crear primera clase y acceder a sus atributos y metodos

---
+++ src/Doctor.java
@@ -0,0 +1,14 @@
+//Clase Doctor
+public class Doctor {
+    //Atributos
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos = Metodos
+
+    public void showName(){
+        System.out.println(name);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //instanciar el objeto
+        Doctor myDoctor = new Doctor();
+        //asignar nombre al doctor
+        myDoctor.name = "Alexander Acevedo";
+        //llamar al metodo
+        myDoctor.showName();
+
+    }
+}
