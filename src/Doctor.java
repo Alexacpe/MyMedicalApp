629ab4f7ba382d7fb67bff086c610b5aeeeabed6
Alex Acevedo <alex.acpe@outlook.com>
2022-06-24 09:22:06 -0500

    Creando constructor y elementos estaticos para acceder a ellos

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,14 +1,26 @@
 //Clase Doctor
 public class Doctor {
     //Atributos
-    int id;
+    static int id = 0; //autoincrementable
     String name;
     String speciality;
+
+
+    //Constructor
+    Doctor(){
+        //System.out.println("Construyento un metodo Doctor");
+    }
+    //Constructor con parametro-> recibe como parametro un nombre de doctor
+   // Doctor(String name){
+        //System.out.println("El nombre del doctor es: "+ name);
+   // }
 
     //Comportamientos = Metodos
 
     public void showName(){
         System.out.println(name);
     }
-
+    public void showId(){
+        System.out.println(id++);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import ui.UIMenu.*;
+
+import static ui.UIMenu.showMenu;
+
 public class Main {
     public static void main(String[] args) {
         //instanciar el objeto
         Doctor myDoctor = new Doctor();
         //asignar nombre al doctor
-        myDoctor.name = "Alexander Acevedo";
+         myDoctor.name = "Alexander Acevedo";
         //llamar al metodo
-        myDoctor.showName();
+        myDoctor.showName();//metodo que muestra nombre
+        myDoctor.showId();//metodo que muestra ID
+        System.out.println(Doctor.id); //imprimir id
 
+
+        //incrementa numero de id con cada objeto Tipo Doctor que se crea
+        Doctor myDoctorCarla = new Doctor(); //Creando objeto p'ara doctor carla
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        //Llamar al menu
+        showMenu();
     }
 }
