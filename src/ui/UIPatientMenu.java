790b7a16e649daccc4b41e25671f136f7e40e99e
Alex Acevedo <alex.acpe@outlook.com>
2022-07-05 10:13:43 -0500

    Crear y recorrer estructuras de datos

--- src/ui/UIPatientMenu.java
+++ src/ui/UIPatientMenu.java
@@ -1,15 +1,20 @@
 package ui;
 
+import model.Doctor;
+
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class UIPatientMenu {
     //metodo que sale cuando el paciente se loggea
-    public static void showPatientMenu(){
+    public static void showPatientMenu() {
         int response = 0;
         do {
             System.out.println("\n\n");
             System.out.println("Patient");
-            System.out.println("Welcome patient: "+ UIMenu.patientLogged);
+            System.out.println("Welcome patient: " + UIMenu.patientLogged);
             //opciones para el paciente
             System.out.println("1. Book an appointmet");
             System.out.println("2. My appointment");
@@ -18,7 +23,7 @@ public class UIPatientMenu {
             Scanner sc = new Scanner(System.in);
             response = Integer.valueOf(sc.nextLine());
 
-            switch (response){
+            switch (response) {
                 case 1:
                     break;
                 case 2:
@@ -26,15 +31,38 @@ public class UIPatientMenu {
                 case 0:
                     UIMenu.showMenu();
             }
-        }while (response!=0);
+        } while (response != 0);
     }
-    private static void showBookAppointmetMenu(){
+
+    private static void showBookAppointmetMenu() {
         int response = 0;
         do {
             System.out.println(":: Book an appointmen");
             System.out.println(":: Select date: ");
+            //mostrar las fechas de los doctores que tienen fechas disponibles
+            //integer para numerar la lista de fechas
+            Map<Integer, Map<Integer, Doctor>> doctors = new TreeMap<>();//segundo indice para numerar la fecha que selecciona el paciente
 
+            //recorrer la lista de citas del doctor
+            int k = 0;
+            for (int i = 0; i < UIDoctorMenu.doctorsAvailableAppointments.size(); i++) {
+                ArrayList<Doctor.AvailableAppointment> availableAppointments = UIDoctorMenu.doctorsAvailableAppointments.get(i).getAvailableAppointments();//al encontrar una cita se guarda en el objeto available appointmets
 
-        }while (response!=0);
+                Map<Integer, Doctor> doctorAppointents = new TreeMap<>();
+
+                for (int j = 0; j < availableAppointments.size(); j++) {
+                    k++;
+                    System.out.println(k + "." + availableAppointments.get(j).getDate());
+                    //insertar el indice de los doctores al map
+                    doctorAppointents.put(Integer.valueOf(j),UIDoctorMenu.doctorsAvailableAppointments.get(i));
+                    //dentro de los doctores esta el map de las fechas de cada uno con su indice
+                    doctors.put(Integer.valueOf(k),doctorAppointents);
+                }
+            }
+            Scanner sc = new Scanner(System.in);
+            int responseDateSelected = Integer.valueOf(sc.nextLine());
+        } while (response != 0);
+
     }
 }
+
